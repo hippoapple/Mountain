666ef38128090c4052cb73576d2d8cb37913ff6b
hippoapple <39970069+hippoapple@users.noreply.github.com>
2020-11-12 05:11:33 +0900

    20201112_JK

---
+++ Assets/02. Scripts/CBS/HandTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandTest : MonoBehaviour
+{
+
+    Vector3 prePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prePos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(transform.position);
+    }
+}
